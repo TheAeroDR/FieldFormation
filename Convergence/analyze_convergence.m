2586d054c72c60b85fcd7fdf91a2f8c44240444a
TheAeroDR <43843333+TheAeroDR@users.noreply.github.com>
2025-07-10 19:56:21 +0100

    Update analyze_convergence.m

--- Convergence/analyze_convergence.m
+++ Convergence/analyze_convergence.m
@@ -38,7 +38,7 @@ for i = 1:length(field_names)
 
 end
 
-n_runs = unique(loaded_data.N1000_timeseries.run);
+n_runs = length(unique(loaded_data.N1000_timeseries.run));
 
 boxy_bx = NaN(n_runs, length(particle_counts));
 boxy_by = NaN(n_runs, length(particle_counts));
