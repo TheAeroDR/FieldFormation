bea0fc233b1374369155ae0e065aaacb9d336348
david <43843333+TheAeroDR@users.noreply.github.com>
2025-09-03 13:52:07 +0100

    updated thesis plots

--- Results/Sensitivity_Analysis/bonus_plots_thesis.m
+++ Results/Sensitivity_Analysis/bonus_plots_thesis.m
@@ -18,8 +18,9 @@ plot(mids,bin_n)
 hold on
 plot(mids,bin_r)
 plot(mids,bin_i)
-legend('Baseline', 'Reduced Radius, 0.9', 'Increased Radius, 1.1')
-
+legend('Baseline', 'Reduced Radius, 0.9', 'Increased Radius, 1.1','Location','southwest')
+xlabel('$\mathcal{R}$ [m]')
+ylabel('Frequency [n]')
 %%
 a = rand(10000,1);
 R = 3.5;
@@ -45,3 +46,33 @@ h3 = plot(baseline.*sin(2.2*pi*b),baseline.*cos(2.2*pi*b),'.','Color',colours(3,
 xlabel('$x$ [m]')
 title('Overlap, $2.2\pi$')
 axis square
+
+%%
+power = log(1/3)/log(0.5);
+min = 0;
+max = 14.7;
+for i = 1:1000
+c = (i/(1000-1)).^power;
+d(i) = min + (max - min) * c;
+end
+figure
+plot(d)
+hold on
+
+power = log(1/10)/log(0.5);
+for i = 1:1000
+c = (i/(1000-1)).^power;
+d(i) = min + (max - min) * c;
+end
+plot(d)
+
+power = log(1/2)/log(0.5);
+for i = 1:1000
+c = (i/(1000-1)).^power;
+d(i) = min + (max - min) * c;
+end
+plot(d)
+
+xlabel('Height array index[-]')
+ylabel('$h$ [m]')
+legend('Baseline, $\frac{1}{3}h_{max}$','Reduced Mean, $\frac{1}{10}h_{max}$','Increased Mean, $\frac{1}{2}h_{max}$','Location','southeast')
